6dff1543db1e86d0c0a7cea6a9710c2351231046
shailus1029 <shailus1029@gmail.com>
2020-04-13 21:49:48 +0530

    node server created

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// intilization our express app;
+const app = express();
+app.use(express.json());
+
+//adding body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//defining the port to run the server
+const port = 8000;
+
+//listening to the port 8000
+const server = app.listen(port, () => {
+	console.log("server is running on  port : ", port);
+});
+
+module.exports = {
+	server,
+};
